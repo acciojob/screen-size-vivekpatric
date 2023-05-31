d8e80f975ee64b9c777d1d8d14011729840b0014
Vivek Patric R <96299357+vivekpatric@users.noreply.github.com>
2023-05-31 23:52:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 //your JS code here. If required.
-function getSize()
-{
-var w = document.documentElement.clientWidth;
-var h = document.documentElement.clientHeight;
-        
-// put the result into a h1 tag
- document.getElementById('wh').innerHTML = "<h1>Width: " + w + " and Height: " + h + "</h1>";
+let screensize=document.getElementById("sizeInfo");
+document.body.append(screensize);
+ // window.onresize = function(){
+	//  sizeElement.innerText = `Width: ${window.innerWidth} and Height: ${window.innerHeight}`;
+ // }
+window.onresize =function () {
+  screensize.innerText=`Width:${window.innerWidth} and Height:${window.innerHeight}`;
 }
