798a1b4015ce191366fea4ca0ffe0499534a0240
Vivek Patric R <96299357+vivekpatric@users.noreply.github.com>
2023-05-31 23:34:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
 //your JS code here. If required.
+function getsize() {
+	var w=document.documentElement.clientWidth;
+	var h=document.documentElement.clientHeight;
+	document.getElementById('width').innerHTML="<h1>Width:"+w+"and Height:"+h+"</h1>; 
+}
