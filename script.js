8bb8f6efeb61a677bb3f7c5dd9fa4f83b721cd6d
Vivek Patric R <96299357+vivekpatric@users.noreply.github.com>
2023-06-01 00:35:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,14 @@
 //your JS code here. If required.
 //your JS code here. If required.
-const sizeElement = document.getElementById("sizeInfo");
+// const sizeElement = document.getElementById("sizeInfo");
 
 
-document.body.append(sizeElement);
- window.onresize = function(){
-	 sizeElement.innerText = `Width: ${window.innerWidth} and Height: ${window.innerHeight}`;
- }
+// document.body.append(sizeElement);
+//  window.onresize = function(){
+// 	 sizeElement.innerText = `Width: ${window.innerWidth} and Height: ${window.innerHeight}`;
+//  }
+function getSize() {
+			var w = document.documentElement.clientWidth;
+			var h = document.documentElement.clientHeight;
+			document.getElementById('sizeInfo').innerHTML = "<h1>Width: " + w + " and Height: " + h + "</h1>";
+		}
