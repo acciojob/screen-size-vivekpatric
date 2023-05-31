647d2fa2756f7b6c02e99029e30fd7f76d9548d1
Vivek Patric R <96299357+vivekpatric@users.noreply.github.com>
2023-05-31 23:39:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 //your JS code here. If required.
-function getsize() {
-	var w=document.documentElement.clientWidth;
-	var h=document.documentElement.clientHeight;
-	document.getElementById('width').innerHTML="<h1>Width:"+w+"and Height:"+h+"</h1>; 
+function getSize()
+{
+var w = document.documentElement.clientWidth;
+var h = document.documentElement.clientHeight;
+        
+// put the result into a h1 tag
+ document.getElementById('wh').innerHTML = "<h1>Width: " + w + " and Height: " + h + "</h1>";
 }
